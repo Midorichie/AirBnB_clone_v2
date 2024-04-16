858ac88d7a54ed5906668c3e3a98f42a996dd91f
Midorichie <hamsohood@gmail.com>
2024-04-16 10:16:52 +0100

    Update

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -71,3 +71,70 @@ class test_fileStorage(unittest.TestCase):
             loaded = obj
         self.assertEqual(new.to_dict()['id'], loaded.to_dict()['id'])
 
+    def test_reload_empty(self):
+        """ Load from an empty file """
+        with open('file.json', 'w') as f:
+            pass
+        with self.assertRaises(ValueError):
+            storage.reload()
+
+    def test_reload_from_nonexistent(self):
+        """ Nothing happens if file does not exist """
+        self.assertEqual(storage.reload(), None)
+
+    def test_base_model_save(self):
+        """ BaseModel save method calls storage save """
+        new = BaseModel()
+        new.save()
+        self.assertTrue(os.path.exists('file.json'))
+
+    def test_type_path(self):
+        """ Confirm __file_path is string """
+        self.assertEqual(type(storage._FileStorage__file_path), str)
+
+    def test_type_objects(self):
+        """ Confirm __objects is a dict """
+        self.assertEqual(type(storage.all()), dict)
+
+    def test_key_format(self):
+        """ Key is properly formatted """
+        new = BaseModel()
+        _id = new.to_dict()['id']
+        for key in storage.all().keys():
+            temp = key
+        self.assertEqual(temp, 'BaseModel' + '.' + _id)
+
+    def test_storage_var_created(self):
+        """ FileStorage object storage created """
+        print(type(storage))
+        self.assertEqual(type(storage), FileStorage)
+
+    def test_all_with_class(self):
+        """Test all() method with a class passed"""
+        new_state = State()
+        new_state.name = "California"
+        storage.new(new_state)
+        storage.save()
+
+        all_state = storage.all(State)
+        count = len(all_state)
+
+        self.assertEqual(1, count)
+
+        # Adding another state
+        new_state = State()
+        new_state.name = "Nevada"
+        storage.new(new_state)
+        storage.save()
+
+        all_state = storage.all(State)
+        count = len(all_state)
+
+        self.assertEqual(2, count)
+
+        # when no instance when created
+        storage.all() == {}
+        state_obj = storage.all(State)
+        self.assertEqual(len(state_obj), 2)
+
+    # def test_delete_when_no_argument_is_pass():
